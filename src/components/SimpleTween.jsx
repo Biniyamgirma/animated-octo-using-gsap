5c2942a474dc87a4b1a079c57803e13a232d4e10
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-08-20 18:21:40 +0300

    added a modern way of handling the data

--- src/components/SimpleTween.jsx
+++ src/components/SimpleTween.jsx
@@ -1,10 +1,11 @@
 import React, {useEffect , useRef} from 'react';
+import {useGSAP} from '@gsap/react';
 import gsap from 'gsap';
 
 const SimpleTween = ()=>{
-    const boxRef = useRef(null);
-    useEffect(()=>{
-        gsap.to(boxRef.current,{
+    const container = useRef(null);
+    useGSAP(()=>{
+     gsap.to('.box',{
             x:600,
             duration:2,
             rotation:360,
@@ -13,11 +14,12 @@ const SimpleTween = ()=>{
             yoyo:true,
             ease:"power1.inOut"
         })
-    },[])
+    },{scop:container})
+    
     return(
-        <div className="p-8">
-            <h1 className="text-2xl mb-4 text-center text-white">goTo() tween</h1>
-            <div ref={boxRef} className="w-24 h-24 bg-cyan-500 rounded-lg"></div>
+        <div ref={container} className="p-8">
+            <h1 className="text-2xl mb-4 text-center text-white">modern way of gsap.to() tween</h1>
+            <div  className="box w-24 h-24 bg-cyan-500 rounded-lg"></div>
         </div>
     )
 
