c633d5a4c9475ed685088d6bd9309913c2767310
Biniyamgirma <119936137+Biniyamgirma@users.noreply.github.com>
2025-08-31 09:43:06 +0300

    finalize my projects

--- src/components/DrawSvg.jsx
+++ src/components/DrawSvg.jsx
@@ -19,23 +19,26 @@ const DrawSVG = ()=>{
 
         gsap.to(checkmark,{
             strokeDashoffset:0,
-            duration:2,
+            duration:.5,
             ease:'power1.inOut',
             scrollTrigger:{
                 trigger:container.current,
                 start:'top center',
                 toggleActions:'play reverse play reverse',
+                markers:true,
             }
         })
     },{scope:container});
 
     return(
-        <div className="h-screen flex flex-col items-center justify-center bg-gray-900">
+        <>
+        
+        <div ref={container} className="h-screen flex flex-col items-center justify-center bg-gray-900">
             <h2 className="text-3xl mb-8 text-amber-50">SVG Line Drawing</h2>
             <svg width="150" height="150" viewBox='0 0 52 52'>
                 <path 
                 ref={checkmarkRef}
-                d="M14.1 27.217.1 7.2 16.7-16.8"
+                d="M14.1 27.2l7.1 7.2 16.7-16.8"
                 fill='none'
                 stroke='rgb(34 197 94)'
                 strokeWidth='5'
@@ -44,6 +47,8 @@ const DrawSVG = ()=>{
                 />
             </svg>
         </div>
+        </>
+        
     )
 } 
 export default DrawSVG;
